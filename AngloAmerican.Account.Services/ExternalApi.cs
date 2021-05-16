fca095c9dca5ce89db239960ce73a3011af82c1e
Prasad Ramireddy <pramireddy08@gmail.com>
2021-05-16 11:51:38 +0100

    Minor change in External api

--- AngloAmerican.Account.Services/ExternalApi.cs
+++ AngloAmerican.Account.Services/ExternalApi.cs
@@ -6,27 +6,19 @@ namespace AngloAmerican.Account.Services
 {
     public class ExternalApi : IExternalApi
     {
-        private readonly List<string> _names = new List<string> { "Rene", "Kirk", "Escarole" };
+        private readonly List<string> _lastNames = new List<string> { "Rene", "Kirk", "Escarole" };
 
         /// <summary>
+        /// if thr person is not in the list return true
         /// If the person is in the list and balance is greater than 10,000 return false
         /// </summary>
         /// <param name="amount"></param>
         /// <param name="lastName"></param>
-        /// <returns>true if person is in list and the balance is valid</returns>
+        /// <returns>true if balance is valid</returns>
         public bool CheckAccountBalance(int amount, string lastName)
         {
-            bool isFalse = false;
-
-            foreach (var n in _names)
-            {
-                isFalse = true;
-                if (n == lastName)
-                {
-                    return amount <= 10000;
-                }
-            }
-            return isFalse;
+            return !_lastNames.Any(x => x.Contains(lastName)) ||
+                  _lastNames.Any(x => x.Contains(lastName) && amount <= 10000);
         }
     }
 }
